bfbd2457964d3fc92edcef94575ca6fd873f765a
Geofany Galindra <37582643+geofany@users.noreply.github.com>
2019-12-01 20:22:17 +0700

    Initial commit

---
+++ Assets/Scripts/CreateObject.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreateObject : MonoBehaviour
+{
+    public float jeda = 0.8f;
+    float timer;
+    public GameObject[] persons;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+        if (timer > jeda)
+        {
+            int random = Random.Range(0, persons.Length);
+            Instantiate(persons[random], transform.position, transform.rotation);
+            timer = 0;
+        }
+    }
+}
---
+++ Assets/Scripts/DetectPersons.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DetectPersons : MonoBehaviour
+{
+    public string nameTag;
+    public AudioClip audioBenar;
+    public AudioClip audioSalah;
+    private AudioSource MediaPlayerBenar;
+    private AudioSource MediaPlayerSalah;
+    public Text textScore;
+    public Text textLife;
+    // Start is called before the first frame update
+    void Start()
+    {
+        MediaPlayerBenar = gameObject.AddComponent<AudioSource>();
+        MediaPlayerBenar.clip = audioBenar;
+
+        MediaPlayerSalah = gameObject.AddComponent<AudioSource>();
+        MediaPlayerSalah.clip = audioSalah;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag.Equals(nameTag))
+        {
+            Data.score += 25;
+            textScore.text = Data.score.ToString();
+            Destroy(collision.gameObject);
+            MediaPlayerBenar.Play();
+        }
+        else
+        {
+            Data.score -= 5;
+            Data.life -= 1;
+            textScore.text = Data.score.ToString();
+            textLife.text = "Nyawa : " + Data.life.ToString();
+            Destroy(collision.gameObject);
+            MediaPlayerSalah.Play();
+        }
+    }
+}
